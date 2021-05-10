38746d3e2121944926659a84e5a54dfec83f478b
johanzhu <634606335@qq.com>
2021-05-10 10:40:07 +0800

    feat: support load only json and atlas file

--- src/SpineLoader.ts
+++ src/SpineLoader.ts
@@ -51,7 +51,11 @@ class SpineLoader extends Loader<Entity> {
       if (item.urls && this.checkUrls(item.urls)) {
         resource = this.getResouceFromUrls(item.urls);
       }
+<<<<<<< HEAD
 
+=======
+      
+>>>>>>> 8f99366 (feat: support load only json and atlas file)
       let autoLoadTexture: boolean = false;
       let assetManager: AssetManager;
       assetManager = new AssetManager(null, (img) => {
