d85b9fd9aa4e387be061001b292376ba444c525d
johanzhu <johanzhu9527@gmail.com>
2021-07-16 11:08:47 +0800

    fix: render error (#33)

--- src/SpineAnimation.ts
+++ src/SpineAnimation.ts
@@ -57,7 +57,7 @@ export class SpineAnimation extends Script {
     this._skeleton = new Skeleton(skeletonData);
     const animationData = new AnimationStateData(skeletonData);
     this._state = new AnimationState(animationData);
-    this._meshGenerator.buildMesh(this._skeleton, this.setting);
+    this._meshGenerator.initialize(this._skeleton);
   }
 
   disposeCurrentSkeleton() {
