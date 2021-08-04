914ea90bd09ceeba5dabe33cbe0e92176472a528
johanzhu <634606335@qq.com>
2021-08-04 16:53:37 +0800

    fix: cr problem

--- example/outline/OutlineAbility.ts
+++ example/outline/OutlineAbility.ts
@@ -20,8 +20,8 @@ export class OutlineAbility extends MeshRenderer {
 
   constructor(entity: Entity) {
     super(entity);
-
-    const mesh = new ModelMesh(this._engine);
+    const engine = this.engine;
+    const mesh = new ModelMesh(engine);
     const positions = [];
     positions.push(
       new Vector3(0, 0, 0),
@@ -42,7 +42,7 @@ export class OutlineAbility extends MeshRenderer {
     mesh.uploadData(false);
 
     this.mesh = mesh;
-    this.setMaterial(createGeometryMaterial(this.engine));
+    this.setMaterial(createGeometryMaterial(engine));
     this.entity.isActive = false;
   }
 
@@ -90,24 +90,35 @@ export class OutlineAbility extends MeshRenderer {
 
     const mesh = <ModelMesh>this.mesh;
     const positions = mesh.getPositions();
-    positions[0].setValue(min.x, min.y, max.z);
-    positions[1].setValue(max.x, min.y, max.z);
-    positions[2].setValue(max.x, min.y, min.z);
-    positions[3].setValue(min.x, min.y, min.z);
-    positions[4].setValue(min.x, max.y, max.z);
-    positions[5].setValue(max.x, max.y, max.z);
-    positions[6].setValue(max.x, max.y, min.z);
-    positions[7].setValue(min.x, max.y, min.z);
+    const { 
+      x: minX,
+      y: minY,
+      z: minZ
+    } = min;
+    const { 
+      x: maxX,
+      y: maxY,
+      z: maxZ
+    } = max;
+    positions[0].setValue(minX, minY, maxZ);
+    positions[1].setValue(maxX, minY, maxZ);
+    positions[2].setValue(maxX, minY, minZ);
+    positions[3].setValue(minX, minY, minZ);
+    positions[4].setValue(minX, maxY, maxZ);
+    positions[5].setValue(maxX, maxY, maxZ);
+    positions[6].setValue(maxX, maxY, minZ);
+    positions[7].setValue(minX, maxY, minZ);
     mesh.setPositions(positions);
     mesh.uploadData(false);
     this.entity.isActive = true;
   }
 
   private addEventListeners() {
+    const { flags } = this;
     const addListener = (component: Component) => {
-      this.flags.push(component.entity?.transform.registerWorldChangeFlag());
+      flags.push(component.entity?.transform.registerWorldChangeFlag());
     };
-    this.flags.length = 0;
+    flags.length = 0;
     this.renderers.forEach(addListener);
   }
 
@@ -116,8 +127,10 @@ export class OutlineAbility extends MeshRenderer {
   }
 
   _render(camera: Camera) {
-    for (let i = 0; i < this.flags.length; i++) {
-      const flag = this.flags[i];
+    const { flags } = this;
+    const flagLength = flags.length;
+    for (let i = 0; i < flagLength; i++) {
+      const flag = flags[i];
       if (flag.flag) {
         this.updateVertices();
         flag.flag = false;
